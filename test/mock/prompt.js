e8278bfd45c053c416658ba7bbb5812083974f25
Josh Farrant <josh@farrant.me>
2016-01-13 16:14:18 +0000

    Removed incorrect property from prompt mock

--- test/mock/prompt.js
+++ test/mock/prompt.js
@@ -5,7 +5,6 @@ const prompts = [{
   parent: 'http://pbx.sipcentric.com/api/v1/customers/5',
   created: '2016-01-01T12:00:00Z',
   name: 'Main welcome',
-  phoneNumber: '03303303330',
   size: 129612
 }, {
   type: 'prompt',
@@ -14,7 +13,6 @@ const prompts = [{
   parent: 'http://pbx.sipcentric.com/api/v1/customers/5',
   created: '2016-01-02T13:00:00Z',
   name: 'Out of hours message',
-  phoneNumber: '01234567890',
   size: 175402
 }, {
   type: 'prompt',
@@ -23,7 +21,6 @@ const prompts = [{
   parent: 'http://pbx.sipcentric.com/api/v1/customers/5',
   created: '2016-01-03T14:00:00Z',
   name: 'IVR options',
-  phoneNumber: '07777777777',
   size: 78478
 }];
 
